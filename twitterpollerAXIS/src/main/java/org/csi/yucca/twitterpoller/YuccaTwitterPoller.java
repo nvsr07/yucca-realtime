41e6b0ce5297f40b893fe5b7048a6e998b4e8752
fcorsan <fabrizio.corsanego@csi.it>
2016-03-25 12:40:17 +0100

    twitter poller, log optimization

--- twitterpollerAXIS/src/main/java/org/csi/yucca/twitterpoller/YuccaTwitterPoller.java
+++ twitterpollerAXIS/src/main/java/org/csi/yucca/twitterpoller/YuccaTwitterPoller.java
@@ -1,7 +1,7 @@
 package org.csi.yucca.twitterpoller;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 import org.csi.yucca.twitterpoller.business.TwitterInvoker;
 import org.csi.yucca.twitterpoller.dto.YuccaInvokeResult;
@@ -14,46 +14,46 @@ import org.csi.yucca.twitterpoller.mongo.YuccaTwitterMongoDataAcces;
 import twitter4j.TwitterException;
 
 public class YuccaTwitterPoller{
-	private static final Logger log=Logger.getLogger("org.csi.yucca.twitterpoller");
+	private static final Log log=LogFactory.getLog("org.csi.yucca.twitterpoller");
 
 	
 	
 	public String twtEcho(String pippo) throws Exception{
-		log.log(Level.INFO, "[YuccaTwitterPoller::twtEcho] BEGIN ");
+		log.info("[YuccaTwitterPoller::twtEcho] BEGIN ");
 		return "echo from "+pippo;
 		
 	}
 	public YuccaTwitterCepRecord invokeTwitter(YuccaTwitterQuery twitterQuery, YuccaTwitterStreamConfig streamInfo) throws Exception{
-		log.log(Level.INFO, "[YuccaTwitterPoller::invokeTwitter] BEGIN ");
+		log.info("[YuccaTwitterPoller::invokeTwitter] BEGIN ");
 		
 		try {
 			
 		
-		log.log(Level.INFO, "[YuccaTwitterPoller::invokeTwitter] streamInfo.getStreamCode " +streamInfo.getStreamCode());
-		log.log(Level.INFO, "[YuccaTwitterPoller::invokeTwitter] streamInfo.getTenatcode " +streamInfo.getTenatcode());
-		log.log(Level.INFO, "[YuccaTwitterPoller::invokeTwitter] streamInfo.getVirtualEntityCode " +streamInfo.getVirtualEntityCode());
-		log.log(Level.INFO, "[YuccaTwitterPoller::invokeTwitter] streamInfo.getResetLastId " +streamInfo.getResetLastId());
-		log.log(Level.INFO, "[YuccaTwitterPoller::invokeTwitter] streamInfo.getStreamVersion " +streamInfo.getStreamVersion());
+		log.info("[YuccaTwitterPoller::invokeTwitter] streamInfo.getStreamCode " +streamInfo.getStreamCode());
+		log.info("[YuccaTwitterPoller::invokeTwitter] streamInfo.getTenatcode " +streamInfo.getTenatcode());
+		log.info("[YuccaTwitterPoller::invokeTwitter] streamInfo.getVirtualEntityCode " +streamInfo.getVirtualEntityCode());
+		log.info("[YuccaTwitterPoller::invokeTwitter] streamInfo.getResetLastId " +streamInfo.getResetLastId());
+		log.info("[YuccaTwitterPoller::invokeTwitter] streamInfo.getStreamVersion " +streamInfo.getStreamVersion());
 
 		
 		
-		log.log(Level.INFO, "[YuccaTwitterPoller::invokeTwitter] twitterQuery.getTwtGeolocUnit " +twitterQuery.getTwtGeolocUnit());
-		log.log(Level.INFO, "[YuccaTwitterPoller::invokeTwitter] twitterQuery.getTwtLang " +twitterQuery.getTwtLang());
-		log.log(Level.INFO, "[YuccaTwitterPoller::invokeTwitter] twitterQuery.getTwtQuery " +twitterQuery.getTwtQuery());
-		log.log(Level.INFO, "[YuccaTwitterPoller::invokeTwitter] twitterQuery.getTwtResultType " +twitterQuery.getTwtResultType());
-		log.log(Level.INFO, "[YuccaTwitterPoller::invokeTwitter] twitterQuery.getTwtTokenSecret " +twitterQuery.getTwtTokenSecret());
-		log.log(Level.INFO, "[YuccaTwitterPoller::invokeTwitter] twitterQuery.getTwtUntil " +twitterQuery.getTwtUntil());
-		log.log(Level.INFO, "[YuccaTwitterPoller::invokeTwitter] twitterQuery.getTwtUserToken " +twitterQuery.getTwtUserToken());
-		log.log(Level.INFO, "[YuccaTwitterPoller::invokeTwitter] twitterQuery.getTwtGeolocLat " +twitterQuery.getTwtGeolocLat());
-		log.log(Level.INFO, "[YuccaTwitterPoller::invokeTwitter] twitterQuery.getTwtGeolocLon " +twitterQuery.getTwtGeolocLon());
-		log.log(Level.INFO, "[YuccaTwitterPoller::invokeTwitter] twitterQuery.getTwtGeolocRadius " +twitterQuery.getTwtGeolocRadius());
-		log.log(Level.INFO, "[YuccaTwitterPoller::invokeTwitter] twitterQuery.getTwtLastSearchId " +twitterQuery.getTwtLastSearchId());
+		log.info("[YuccaTwitterPoller::invokeTwitter] twitterQuery.getTwtGeolocUnit " +twitterQuery.getTwtGeolocUnit());
+		log.info("[YuccaTwitterPoller::invokeTwitter] twitterQuery.getTwtLang " +twitterQuery.getTwtLang());
+		log.info("[YuccaTwitterPoller::invokeTwitter] twitterQuery.getTwtQuery " +twitterQuery.getTwtQuery());
+		log.info("[YuccaTwitterPoller::invokeTwitter] twitterQuery.getTwtResultType " +twitterQuery.getTwtResultType());
+		log.info("[YuccaTwitterPoller::invokeTwitter] twitterQuery.getTwtTokenSecret " +twitterQuery.getTwtTokenSecret());
+		log.info("[YuccaTwitterPoller::invokeTwitter] twitterQuery.getTwtUntil " +twitterQuery.getTwtUntil());
+		log.info("[YuccaTwitterPoller::invokeTwitter] twitterQuery.getTwtUserToken " +twitterQuery.getTwtUserToken());
+		log.info("[YuccaTwitterPoller::invokeTwitter] twitterQuery.getTwtGeolocLat " +twitterQuery.getTwtGeolocLat());
+		log.info("[YuccaTwitterPoller::invokeTwitter] twitterQuery.getTwtGeolocLon " +twitterQuery.getTwtGeolocLon());
+		log.info("[YuccaTwitterPoller::invokeTwitter] twitterQuery.getTwtGeolocRadius " +twitterQuery.getTwtGeolocRadius());
+		log.info("[YuccaTwitterPoller::invokeTwitter] twitterQuery.getTwtLastSearchId " +twitterQuery.getTwtLastSearchId());
 		
 		
 		YuccaTwitterMongoDataAcces mongoDAO=new YuccaTwitterMongoDataAcces();
 		
 		Long lastId=mongoDAO.retrieveLastTweetId(streamInfo.getStreamCode(), streamInfo.getTenatcode(), streamInfo.getVirtualEntityCode(),streamInfo.getStreamVersion(),(1==streamInfo.getResetLastId()));
-		log.log(Level.INFO, "[YuccaTwitterPoller::invokeTwitter] lastId recuperato -->"+lastId);
+		log.info("[YuccaTwitterPoller::invokeTwitter] lastId recuperato -->"+lastId);
 		
 		
 		if (null!=lastId) twitterQuery.setTwtLastSearchId(lastId);
@@ -70,10 +70,10 @@ public class YuccaTwitterPoller{
 		ret.setValues(resultChiamata.getValuesRet());
 		
 		
-		log.log(Level.INFO, "[YuccaTwitterPoller::invokeTwitter] lastId da twitter -->"+lastId);
+		log.info("[YuccaTwitterPoller::invokeTwitter] lastId da twitter -->"+lastId);
 
-		log.log(Level.INFO, "[YuccaTwitterPoller::invokeTwitter] LAT -->"+twitterQuery.getTwtGeolocLat());
-		log.log(Level.INFO, "[YuccaTwitterPoller::invokeTwitter] LON -->"+twitterQuery.getTwtGeolocLon());
+		log.info("[YuccaTwitterPoller::invokeTwitter] LAT -->"+twitterQuery.getTwtGeolocLat());
+		log.info("[YuccaTwitterPoller::invokeTwitter] LON -->"+twitterQuery.getTwtGeolocLon());
 
 		
 		if (lastId!=-1)  mongoDAO.updateLastId(streamInfo.getStreamCode(), streamInfo.getTenatcode(), streamInfo.getVirtualEntityCode(),lastId);
@@ -85,7 +85,7 @@ public class YuccaTwitterPoller{
 		return ret;
 		
 		} catch (TwitterException twe) {
-			log.log(Level.SEVERE, "[YuccaTwitterPoller::invokeTwitter] ERROR: "+twe);
+			log.error("[YuccaTwitterPoller::invokeTwitter] ERROR: "+twe);
 
 			
 			YuccaTwitterException ecc= new YuccaTwitterException();
@@ -96,7 +96,7 @@ public class YuccaTwitterPoller{
 			
 			return ret;
 		} finally {
-			log.log(Level.INFO, "[YuccaTwitterPoller::invokeTwitter] END ");
+			log.info("[YuccaTwitterPoller::invokeTwitter] END ");
 
 		}
 	
