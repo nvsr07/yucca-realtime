4f0ac757a0529362048f7ce532553c9746f0dcee
Claudio Parodi <gigaclood@gmail.com>
2016-08-09 12:48:05 +0200

    Integration test: first version with autentication

---
+++ integration-test/src/test/java/org/csi/yucca/realtime/test/HttpCEPInsertTest.java
@@ -0,0 +1,121 @@
+package org.csi.yucca.realtime.test;
+
+import static io.restassured.RestAssured.given;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Properties;
+
+import org.apache.commons.lang3.StringUtils;
+import org.hamcrest.Matchers;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
+
+public class HttpCEPInsertTest {
+
+	JSONObject secretObject = new JSONObject();
+
+	@BeforeClass
+	public void setUpSecretObject() throws IOException {
+		String str = readFile("/testSecret.json");
+		secretObject=  new JSONObject(str);
+		
+	}	
+	
+  
+	
+  @DataProvider(name="httpCEPInsertData")
+  public Iterator<Object[]> getFromJson()
+	{
+
+		ArrayList<Object[]> data = new ArrayList<>();
+		
+		String str = readFile("/dataIn.json");
+		JSONObject json =  new JSONObject(str);
+		JSONArray jsArray = json.getJSONArray("data");
+		
+		for (int i = 0; i < jsArray.length(); i++) {
+			JSONObject arr = jsArray.getJSONObject(i);
+			
+			// merge with secret
+			
+			Iterator iterSecret = secretObject.keys();
+			String tmp_key;
+			while(iterSecret.hasNext()) {
+			    tmp_key = (String) iterSecret.next();
+			    if (!arr.has(tmp_key))
+			    {
+			    	arr.put(tmp_key, secretObject.get(tmp_key));
+			    }
+			}
+			
+			
+			data.add(new Object[]{arr});
+		}
+		
+		
+		
+		return data.iterator();
+	}
+  
+  
+  
+  
+  @Test(dataProvider="httpCEPInsertData")
+  public void sendHTTPStatusErrorCodeTesting(JSONObject dato) {
+	RequestSpecification rs =  given().body(dato.get("rt.message")).contentType(ContentType.JSON);
+
+	if (StringUtils.isNotEmpty(dato.optString("rt.username")))
+	{
+		rs = rs.auth().basic(dato.getString("rt.username"), dato.getString("rt.password"));
+	}
+	
+	Response rsp = rs.when().
+			post(dato.get("rt.url")+"/"+dato.get("rt.tenant")+"/");
+	
+  	rsp.then().
+  		statusCode(dato.getInt("rt.httpStatusExcepted"));
+  	
+  	if (StringUtils.isNotEmpty(dato.optString("rt.errorCode")))
+  		rsp.then().body("error_code", Matchers.equalTo(dato.optString("rt.errorCode")));
+  }
+  
+  
+  
+  private String readFile(String file)
+  {
+      String jsonData = "";
+         BufferedReader br = null;
+         try {
+             String line;
+             InputStream inputStream = this.getClass().getResourceAsStream(file);
+             InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+             br = new BufferedReader(inputStreamReader);
+             while ((line = br.readLine()) != null) {
+                 jsonData += line + "\n";
+             }
+         } catch (IOException e) {
+             e.printStackTrace();
+         } finally {
+             try {
+                 if (br != null)
+                     br.close();
+             } catch (IOException ex) {
+                 ex.printStackTrace();
+             }
+         }
+         return jsonData;
+  }
+}
